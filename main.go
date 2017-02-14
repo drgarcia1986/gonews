d3f878a203b2f93da35a8d76f6a70c0972ac4975
Diego Garcia <drgarcia1986@gmail.com>
2017-02-14 17:22:57 -0200

    change main to dealing with `reddit` provider

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/drgarcia1986/gonews/client"
 	"github.com/drgarcia1986/gonews/gui"
 	"github.com/drgarcia1986/gonews/providers"
 	"github.com/drgarcia1986/gonews/providers/hackernews"
+	"github.com/drgarcia1986/gonews/providers/reddit"
 	"github.com/drgarcia1986/gonews/story"
 	"github.com/drgarcia1986/gonews/utils"
 )
@@ -17,12 +19,15 @@ var (
 	limit            int
 	storyType        int
 	flagStoryType    string
-	flagProviderType string = "hn"
+	flagProviderType string
 )
 
 func init() {
 	flag.IntVar(&limit, "limit", 10, "Number of Stories to get")
 	flag.StringVar(&flagStoryType, "type", "top", "Stories Type ('new' or 'top')")
+	flag.StringVar(
+		&flagProviderType, "provider", "hackernews",
+		"Stories Provider (hackernews, reddit, reddit-<subreddit>)")
 
 	flag.Parse()
 
@@ -39,13 +44,30 @@ func init() {
 }
 
 func getProvider(providerType string) providers.Provider {
-	return hackernews.New()
+	switch providerType {
+	case "hackernews":
+		return hackernews.New()
+	case "reddit":
+		return reddit.New()
+	default:
+		providerArgs := strings.Split(providerType, "-")
+		if len(providerArgs) != 2 || providerArgs[0] != "reddit" {
+			return nil
+		}
+		return reddit.NewSubReddit(providerArgs[1])
+	}
+
+	return nil
 }
 
 func main() {
-	fmt.Println("Getting HackerNews stories")
-
 	p := getProvider(flagProviderType)
+	if p == nil {
+		fmt.Println("Invalid Provider ", flagProviderType)
+		os.Exit(0)
+	}
+
+	fmt.Printf("Getting %s stories\n", p.Name())
 	c := client.NewWithPB(p)
 	stories, err := c.GetStories(storyType, limit)
 	if err != nil {
